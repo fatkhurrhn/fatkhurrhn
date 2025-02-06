5d962cc1ecdccc78d56a5cf03ec9d5b22db12e4d
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-02-06 22:57:21 +0700

    update

---
+++ src/LinkPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LinkPage = () => {
+  return (
+    <div>
+      <h1>Halaman Link</h1>
+      <p>Ini adalah halaman /link.</p>
+    </div>
+  );
+};
+
+export default LinkPage;
