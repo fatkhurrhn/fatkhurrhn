fcb67e1ad52447da7fb153526b4e484630df90c7
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-02-07 23:02:53 +0700

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -54,13 +55,13 @@ function App() {
                   </path>
                 </svg>Home<span
                   className="absolute -bottom-px left-0 right-0 h-px bg-gradient-to-r from-zinc-400/0 via-zinc-400/40 to-zinc-400/0"></span></a>
-              <a className="relative flex items-center gap-2 py-3 pr-1 text-sm transition-colors hover:text-zinc-300 sm:text-base"
-                href="#skills" target="_self" data-astro-prefetch="load"><svg xmlns="http://www.w3.org/2000/svg"
-                  width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                  stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-briefcase">
-                  <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-                  <rect width="20" height="14" x="2" y="6" rx="2"></rect>
-                </svg>Skills</a>
+              <Link to="/certificate" className="relative flex items-center gap-2 py-3 pr-1 text-sm transition-colors hover:text-zinc-300 sm:text-base">
+   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-briefcase">
+      <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+      <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+   </svg>
+   Certificate
+</Link>
               <a className="relative flex items-center gap-2 py-3 pr-1 text-sm transition-colors hover:text-zinc-300 sm:text-base"
                 href="#experiences" target="_self" data-astro-prefetch="load"><svg
                   xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
