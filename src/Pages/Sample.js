3b7164f22594186becdd1666e28afcde56f36cd1
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-03-04 05:36:37 +0700

    update

--- src/Pages/Sample.js
+++ src/Pages/Sample.js
@@ -1,5 +1,3 @@
-import Navbar from "../components/Navbar";
-import FooterComponent from "../components/FooterUtama";
 
 function Sample() {
   // useState hanya dibutuhkan jika Anda menggunakan state di dalam komponen ini
@@ -8,12 +6,10 @@ function Sample() {
   return (
     <div className="relative bg-zinc-900 text-zinc-400 min-h-screen">
       <div className="mx-auto max-w-[850px] px-4 pb-6 pt-6 text-lg sm:px-12 md:px-16">
-        <Navbar />
         <div className="mt-6"> {/* Tambahkan margin atas */}
           {/* Konten utama di sini */}
         </div>
       </div>
-      <FooterComponent />
     </div>
   );
 }
