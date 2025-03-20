240c54eeb95831ca96da17283a3c7639909b21d4
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-03-20 14:34:55 +0700

    update

--- src/Components/NavbarStore.jsx
+++ src/Components/NavbarStore.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <MenuAtas/>
 
                 {/* Contact Me Button */}
-                <a href="https://wa.me/6282285512813" target="_blank"
+                <a href="https://wa.me/6285179917416" target="_blank"
                     type="button"
                     className="text-[#d4d4d4] font-medium rounded-lg text-sm px-4 py-2 dark:bg-[#18181b] border border-[#252529] dark:hover:bg-[#1f1f24]"
                 >
