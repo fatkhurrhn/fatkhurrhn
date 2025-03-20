536cc73a29333cd065d9e18f31ea4b3e84ff595e
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-03-20 13:08:55 +0700

    update

--- src/Components/NavbarStore.jsx
+++ src/Components/NavbarStore.jsx
@@ -28,46 +28,6 @@ const Navbar = () => {
                 </a>
             </div>
 
-            {/* Sidebar */}
-            <div
-                className={`fixed top-0 left-0 h-full w-2/4 bg-[#141417] text-[#d4d4d4] transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-                    } transition-transform duration-300 ease-in-out z-30 px-6`}
-            >
-                {/* Close Button */}
-                <button
-                    onClick={() => setIsSidebarOpen(false)}
-                    className="absolute top-4 right-4 text-2xl"
-                >
-                    <i className="ri-close-line"></i>
-                </button>
-
-                {/* Sidebar Menu */}
-                <MenuSidebar/>
-
-                {/* Social Media Icons */}
-                <div className="absolute bottom-5 left-0 w-full flex justify-center gap-4">
-                    <a href="https://youtube.com/fatkhurrhnn" target="_blank" rel="noopener noreferrer">
-                        <i className="ri-youtube-fill text-gray-400 text-xl hover:text-white transition-all"></i>
-                    </a>
-                    <a href="https://linkedin.com/fatkhurrhn" target="_blank" rel="noopener noreferrer">
-                        <i className="ri-linkedin-box-fill text-gray-400 text-xl hover:text-white transition-all"></i>
-                    </a>
-                    <a href="https://tiktok.com/fatkhurrhnn" target="_blank" rel="noopener noreferrer">
-                        <i className="ri-tiktok-fill text-gray-400 text-xl hover:text-white transition-all"></i>
-                    </a>
-                    <a href="https://instagram.com/fatkhurrhn" target="_blank" rel="noopener noreferrer">
-                        <i className="ri-instagram-fill text-gray-400 text-xl hover:text-white transition-all"></i>
-                    </a>
-                </div>
-            </div>
-
-            {/* Overlay Background */}
-            {isSidebarOpen && (
-                <div
-                    className="fixed inset-0 bg-black bg-opacity-50 z-20"
-                    onClick={() => setIsSidebarOpen(false)}
-                ></div>
-            )}
 
             
 
