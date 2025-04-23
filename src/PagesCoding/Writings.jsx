82faf289b0134a5a322ecd01dcf97834b96c3dc8
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-04-23 22:03:08 +0700

    update

--- src/PagesCoding/Writings.jsx
+++ src/PagesCoding/Writings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavbarStore";
 import Footer from "../components/Footer";
 import BlogSection from "../PageSection/Writings/Blogs"
 
