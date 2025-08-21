887f7f98edcab31087a0e41a29ec3617419d46d1
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2025-08-21 07:38:33 +0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -19,24 +19,6 @@ import DetailBlog from './pages/frontdev/DetailBlog';
 import Writings01 from './pages/frontdev/DetailWritings/tailwind-ui-is-now-tailwind-plus';
 import OtherFrontDev from './pages/frontdev/OtherFrontDev';
 
-// ─── Creator Pages ──────────────────────────────────
-import Creator from './pages/creator/HomePage';
-import Storythur from './pages/creator/Storythur';
-import Quotes from './pages/creator/Quotes';
-import Mentahan from './pages/creator/Mentahan';
-import MentahanAudio from './pages/creator/MentahanAudio';
-import MentahanVideo from './pages/creator/MentahanVideo';
-import Other from './pages/creator/Other';
-import Store from './pages/creator/Store';
-
-import StoreMl from './pages/creator/detailStore/DetailDiamondML';
-import StoreNetflix from './pages/creator/detailStore/DetailNetflix';
-import StoreCapcut from './pages/creator/detailStore/DetailCapcut';
-import StoreCanva from './pages/creator/detailStore/DetailCanva';
-import StoreViu from './pages/creator/detailStore/DetailViu';
-import StoreAM from './pages/creator/detailStore/DetailAlightMotion';
-import StoreYT from './pages/creator/detailStore/DetailYoutube';
-
 // ─── Islamic Page ───────────────────────────────────
 import Islamic from './pages/Islamic/Islamic';
 
@@ -85,25 +67,6 @@ function App() {
         <Route path="/frontdev/blogs" element={<Blogs />} />
         <Route path="/frontdev/blogs/:slug" element={<DetailBlog />} />
         <Route path="/frontdev/writings/tailwind-ui-is-now-tailwind-plus" element={<Writings01 />} />
-
-        {/* ── Creator Routes ────────────── */}
-        <Route path="/creator" element={<Creator />} />
-        <Route path="/creator/storythur" element={<Storythur />} />
-        <Route path="/creator/quotes" element={<Quotes />} />
-        <Route path="/creator/mentahan" element={<Mentahan />} />
-        <Route path="/creator/mentahan-audio" element={<MentahanAudio />} />
-        <Route path="/creator/mentahan-video" element={<MentahanVideo />} />
-        <Route path="/creator/other" element={<Other />} />
-
-        {/* ── Creator Store ─────────────── */}
-        <Route path="/creator/store" element={<Store />} />
-        <Route path="/creator/store/diamond-ml" element={<StoreMl />} />
-        <Route path="/creator/store/netflix" element={<StoreNetflix />} />
-        <Route path="/creator/store/capcut" element={<StoreCapcut />} />
-        <Route path="/creator/store/canva" element={<StoreCanva />} />
-        <Route path="/creator/store/viu" element={<StoreViu />} />
-        <Route path="/creator/store/alight-motion" element={<StoreAM />} />
-        <Route path="/creator/store/youtube" element={<StoreYT />} />
 
         {/* ── Islamic Route ─────────────── */}
         <Route path="/islamic" element={<Islamic />} />
